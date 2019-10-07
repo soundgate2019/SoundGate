06b2c0cd482604b22b3adf9d3eb09d0a42ae90a0
Victor Ribeiro <victorlima.12@hotmail.com>
2019-10-07 11:04:10 -0300

    banco

--- src/main/java/br/com/fiap/soundgate/controller/HomeController.java
+++ src/main/java/br/com/fiap/soundgate/controller/HomeController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HomeController {
-    @GetMapping("/Home")
+    @GetMapping("/")
     public String index(){
         return"home.html";
     }
