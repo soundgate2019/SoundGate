354c77999490bfab03b27b737a6b4bef95373248
ElcioPazini <elcio.pazini@hotmail.com>
2019-10-11 15:01:13 -0300

    Consertando as rotas fantasmas

---
+++ src/main/java/br/com/fiap/soundgate/controller/ConsertoController.java
@@ -0,0 +1,47 @@
+package br.com.fiap.soundgate.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ConsertoController {
+    @GetMapping("/usuarios")
+    public String usuarios(){
+        return "usuario/home";
+    }
+    @GetMapping("/usuarios/{id}")
+    public String usuariosPorId(){
+        return "usuario/home";
+    }
+    @GetMapping("/usuarios/{id}/endereco")
+    public String usuariosPorIdEndereco(){
+        return "usuario/home";
+    }
+    @GetMapping("/usuarios/{id}/ingressos")
+    public String usuariosPorIdIngressos(){
+        return "usuario/home";
+    }
+    @GetMapping("/usuarios/{id}/historicos")
+    public String usuariosPorIdHistoricos(){
+        return "usuario/home";
+    }
+    @GetMapping("/empresas")
+    public String empresas(){
+        return "usuario/home";
+    }
+
+    @GetMapping("/empresas/{id}")
+    public String empresasPorId(){
+        return "usuario/home";
+    }
+
+    @GetMapping("/empresas/{id}/endereco")
+    public String empresasPorIdEndereco(){
+        return "usuario/home";
+    }
+    @GetMapping("/empresas/{id}/eventos")
+    public String empresasPorIdEventos(){
+        return "usuario/home";
+    }
+
+}
--- src/main/java/br/com/fiap/soundgate/controller/UsuarioController.java
+++ src/main/java/br/com/fiap/soundgate/controller/UsuarioController.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
@@ -149,13 +150,5 @@ public class UsuarioController {
         Usuario uSes = usuarioRepository.findByCd(u.getCd());
         session.setAttribute("usuario", uSes);
         return "usuario/compra";
-    }
-    @GetMapping("/usuarios")
-    public String usuarios(){
-        return "usuario/home";
-    }
-    @GetMapping("/empresas")
-    public String empresas(){
-        return "usuario/home";
     }
 }
