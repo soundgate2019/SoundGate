b3fbe99c0fee542bdf5a19ed0d469d8d5295e987
Victor Ribeiro <victorlima.12@hotmail.com>
2019-10-18 09:19:58 -0300

    teste

--- src/main/java/br/com/fiap/soundgate/entity/Historico.java
+++ src/main/java/br/com/fiap/soundgate/entity/Historico.java
@@ -35,7 +35,7 @@ public class Historico {
     @ManyToOne()
     @JoinColumn(name="cd_usuario")
     private Usuario usuario;
-    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")
     @Temporal(TemporalType.DATE)
     @Column(name="dt_transacao",nullable=false)
     private Calendar data;
